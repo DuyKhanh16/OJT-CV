8c9d97870171d5cb0f1ce4812c6ed62537b72667
hoang <khuongdanhhoang123@gmail.com>
2024-03-29 14:48:18 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,21 @@ import { AppService } from './app.service';
 
 import { CompanyModule } from './company/company.module';
 import { UserModule } from './user/user.module';
-
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [UserModule,CompanyModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true
+  }),
+    UserModule,CompanyModule],
 
   controllers: [AppController],
   providers: [AppService],
